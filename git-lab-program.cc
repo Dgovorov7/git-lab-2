b3e3668a0213357be82e4aafb3c2da296d82d781
Dgovorov7 <dg729219@ohio.edu>
2021-09-03 16:57:03 -0400

    added git lab program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Denis Govorov
+ *        Date: sep 3, 2021
+ * Description: For lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
